fa65ca8d2614e3cb5365f371c9e3fc7522203228
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-09 21:44:00 +0300

    Add validation for models

--- app/models/budget_log.rb
+++ app/models/budget_log.rb
@@ -2,4 +2,7 @@ class BudgetLog < ApplicationRecord
     belongs_to :user
     has_many :category_budget_logs, dependent: :destroy
     has_many :categories, through: :category_budget_logs
+    validates :amount, presence: true,  numericality: { greater_than: 0, only_integer: true }
+    validates :name, presence: true
+    validates :categories, presence: true :on => :create
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :category_budget_logs, dependent: :destroy
     has_many :budget_logs, through: :category_budget_logs
+    validates :name, presence: true
+    validates :icon, presence: true
 end
