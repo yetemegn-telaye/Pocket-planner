a3d3a78805a38396136383fec339c95b47f5fc17
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-09 21:48:22 +0300

    Fix linters

--- app/models/budget_log.rb
+++ app/models/budget_log.rb
@@ -1,8 +1,8 @@
 class BudgetLog < ApplicationRecord
-    belongs_to :user
-    has_many :category_budget_logs, dependent: :destroy
-    has_many :categories, through: :category_budget_logs
-    validates :amount, presence: true,  numericality: { greater_than: 0, only_integer: true }
-    validates :name, presence: true
-    validates :categories, presence: true :on => :create
+  belongs_to :user
+  has_many :category_budget_logs, dependent: :destroy
+  has_many :categories, through: :category_budget_logs
+  validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true }
+  validates :name, presence: true
+  validates :categories, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
-    belongs_to :user
-    has_many :category_budget_logs, dependent: :destroy
-    has_many :budget_logs, through: :category_budget_logs
-    validates :name, presence: true
-    validates :icon, presence: true
+  belongs_to :user
+  has_many :category_budget_logs, dependent: :destroy
+  has_many :budget_logs, through: :category_budget_logs
+  validates :name, presence: true
+  validates :icon, presence: true
 end
