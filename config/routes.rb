671fe78028d92cdc74bfaf1d43d06b009c83daf9
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-10 16:10:32 +0300

    Create splash screen and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  resources :categories do 
+    resources :budget_logs
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   devise_scope :user do
-    get '/users/sign_out', to: 'devise/sessions#destroy'
-    root to: 'devise/sessions#new'
+    get '/logout', to: 'devise/sessions#destroy'
   end
-
-  resources :categories, only: [:index, :show, :new, :create]
+  root to: 'home#index'
 end
