2d765fb5ee6ab5cea68265a8331682c34be70e73
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-11 12:56:06 +0300

    Add join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_085500) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_11_095356) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_085500) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_budget_logs_on_user_id"
+  end
+
+  create_table "budget_logs_categories", id: false, force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "budget_log_id", null: false
   end
 
   create_table "categories", force: :cascade do |t|
