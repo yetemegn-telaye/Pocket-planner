e6399a587946cf044b2ba6d8c3b3c785cefecf5d
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-11 12:46:10 +0300

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :categories do 
-    resources :budget_logs
-  end
   devise_for :users
+
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'home#index', as: :unauthenticated_root
+  end
+
+  resources :categories, only: [:index, :new, :create, :edit, :update, :show, :destroy] do 
+    resources :budget_logs, only: [:index, :new, :create, :edit, :update, :show, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
