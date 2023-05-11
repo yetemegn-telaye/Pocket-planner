647e3e1a65aa5a8d4c68ac3291eb0bba26b74689
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-11 17:09:25 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :categories, only: [:index, :new, :create, :edit, :update, :show, :destroy] do 
-    resources :budget_logs, only: [:index, :new, :create, :edit, :update, :show, :destroy]
+    resources :budget_logs, only: [:new, :create, :edit, :update, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/logout', to: 'devise/sessions#destroy'
   end
-  root to: 'home#index'
+  
 end
